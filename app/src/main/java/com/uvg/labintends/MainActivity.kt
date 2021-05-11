282174ce705ca237c353c5344335283ff3dc478f
Javier19-cmd <63107636+Javier19-cmd@users.noreply.github.com>
2021-05-11 11:50:11 -0600

    aa

--- app/src/main/java/com/uvg/labintends/MainActivity.kt
+++ app/src/main/java/com/uvg/labintends/MainActivity.kt
@@ -3,6 +3,7 @@ package com.uvg.labintends
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,8 +18,24 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("valor", "Hola Mundo")
             intent.putExtra("valor2", "Prueba")
             //intent.putExtra("valor3", "aaa")
-            startActivity(intent)
+            //startActivity(intent)
             //finish()
+
+            startActivityForResult(intent, 1)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode == RESULT_OK){
+            if(requestCode == 1) {
+                val bundle = data?.extras
+                if(bundle != null){
+                    val resultado = bundle.getString("resultado1")
+                    Toast.makeText(this, resultado, Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/uvg/labintends/MainActivity2.kt
+++ app/src/main/java/com/uvg/labintends/MainActivity2.kt
@@ -1,5 +1,6 @@
 package com.uvg.labintends
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -29,6 +30,15 @@ class MainActivity2 : AppCompatActivity() {
             val valor3 = bundle.getString("valor3", "No hay valor")
             Toast.makeText(this, valor3, Toast.LENGTH_SHORT).show()
 
+        }
+
+        btnRegresar.setOnClickListener()
+        {
+
+            val intent: Intent = Intent()
+            intent.putExtra("resultado1", "valor de pantalla 2")
+            setResult(RESULT_OK, intent)
+            finish() //Finalizo la pantalla.
         }
 
     }
